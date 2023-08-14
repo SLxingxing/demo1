f98b0120d8f137be736588b393f4f53bafbbfa7e
SLxingxing <71885777+SLxingxing@users.noreply.github.com>
2023-08-14 19:29:26 +0800

    Add files via upload

--- phoenix/phoenix-admin/src/main/java/com/starlight/service/Impl/DemoService.java
+++ phoenix/phoenix-admin/src/main/java/com/starlight/service/Impl/DemoService.java
@@ -1,5 +1,9 @@
 package com.starlight.service.Impl;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+
 import org.springframework.stereotype.Service;
 
 /**
@@ -11,8 +15,19 @@ import org.springframework.stereotype.Service;
 @Service
 public class DemoService {
 
-    public int method(int num1, int num2){
+    private String fileName = "data.txt";
 
+    private String packagePath = "/Users/shilian/Documents/java/phoenix/fileDemoPackage";
+
+    public int method(int num1, int num2) {
+        int resultNum = num1 + num2;
+        try(FileOutputStream fileInputStream = new FileOutputStream(packagePath + File.separator +fileName);
+        OutputStreamWriter outputStream = new OutputStreamWriter(fileInputStream)){
+            outputStream.write(String.valueOf(resultNum));
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        System.out.println("file input success!");
         return num1 + num2;
     }
 }
